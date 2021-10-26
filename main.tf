470cb48cebe31e25c56f79ad15445fb5b0459e9a
vishalg98 <87966153+vishalg98@users.noreply.github.com>
2021-10-26 15:48:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  # required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-  } 
-}  
+  }
+}
 # Provider Block
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
+}
+terraform {
+   backend "s3" {
+     bucket = "example-terraform--name-bootstrap-terraform-state"
+     key    = "default-infrastructure"
+     region = "us-west-2"
+   }
+ }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "example-terraform--name-bootstrap-terraform-state"
+
+  versioning {
+    enabled = true
+  }
 }
 
